84a07526ee97843718c5946e945bf6077b0090ee
Khalid Ali <khalludi123@gmail.com>
2020-01-15 22:15:16 -0500

    Printing character with fixed position works

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,5 @@
+#include "screen.h"
+
 void main() {
   // Create a pointer to a char, and point it to the first text cell of
   // video memory (top-left of the screen)
@@ -5,4 +7,8 @@ void main() {
   // At the address pointed to by video_memory, store the character 'X'
   // (display 'X' in the top-left of the screen).
   *video_memory = 'X';
+  
+  // Test print a message
+  print_char('A', 3, 3, 0);
+  print_char('A', -1, -1, 0);
 }
