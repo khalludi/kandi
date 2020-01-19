7203e5d2d2d521881a2443c59d6b9da605dc6abd
Khalid Ali <khalludi123@gmail.com>
2020-01-19 10:11:43 -0500

    Fix warnings

--- drivers/screen.c
+++ drivers/screen.c
@@ -1,3 +1,4 @@
+#include "low_level.h"
 #include "screen.h"
 #include "util.h"
 
@@ -117,14 +118,14 @@ int handle_scrolling(int cursor_offset) {
   /* Shuffle the rows back one. */
   int i;
   for (i=1; i<MAX_ROWS; i++) {
-    memory_copy(get_screen_offset(0,i) + VIDEO_ADDRESS,
-                get_screen_offset(0,i-1) + VIDEO_ADDRESS,
+    memory_copy((char *) (get_screen_offset(0,i) + VIDEO_ADDRESS),
+                (char *) (get_screen_offset(0,i-1) + VIDEO_ADDRESS),
                 MAX_COLS*2
     );
   }
 
   /* Blank the last line by setting all bytes to 0 */
-  char* last_line = get_screen_offset(0,MAX_ROWS-1) + VIDEO_ADDRESS;
+  char* last_line = (char *) (get_screen_offset(0,MAX_ROWS-1) + VIDEO_ADDRESS);
   for (i=0; i< MAX_COLS*2; i++) {
     last_line[i] = 0;
   }
