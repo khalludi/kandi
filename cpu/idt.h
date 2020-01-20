97d659d63c6b8fdf5420f7e1b63801dbf0eb98e9
Khalid Ali <khalludi123@gmail.com>
2020-01-20 08:47:27 -0500

    Start implementing interrupts

---
+++ cpu/idt.h
@@ -0,0 +1,33 @@
+#ifndef IDT_H
+#define IDT_H
+
+/* Interrupt gate */
+typedef struct {
+  unsigned short low_offset; /* Lower 16 bits of handler function address */
+  unsigned short sel; /* Kernel segment selector */
+  unsigned char always0;
+  /* First byte
+   * Bit 7: "Interrupt is present"
+   * Bits 6-5: Privilege level of caller (0=kernel..3=user)
+   * Bit 4: Set to 0 for interrupt gates
+   * Bits 3-0: bits 1110 = decimal 14 = "32 bit interrupt gate" */
+  unsigned char flags;
+  unsigned short high_offset; /* Higher 16bits of handler function address */
+} __attribute__((packed)) idt_gate_t;
+
+/* A pointer to the array of interrupt handers.
+ * Assembly instruction 'lidt' will read it */
+typedef struct {
+  unsigned short limit;
+  unsigned int base;
+} __attribute__((packed)) idt_register_t;
+
+#define IDT_ENTRIES 256
+idt_gate_t idt[IDT_ENTRIES];
+idt_register_t idt_reg;
+
+/* Functions implemented in idt.c */
+void set_idt_gate(int n, unsigned int handler);
+void set_idt();
+
+#endif
